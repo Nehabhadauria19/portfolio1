040686bbb5c533c581e0ef7ba707eebbdd44a2d3
Neha Bhadauria <146559822+Nehabhadauria19@users.noreply.github.com>
2024-02-25 10:51:48 +0530

    Update works.jsx

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -13,7 +13,7 @@ const Works = () => {
     <h2 className="worksTitle">MY PROJECTS!</h2>
     <span className="worksDesc">I take pride in paying attention to the smallest details. I am excited to bring my skills and achievements to you.</span>
     <div className="worksImgs">
-   <a href="http://covid-19-viruss.netlify.app" target="_blank" rel="noopener noreferrer"><p className='para'>COVID-19 TRACKER (click)</p><img src={p1} alt="" className='worksImg' /></a>
+   <a href="https://eloquent-hodgkin-9f52a3.netlify.app/" target="_blank" rel="noopener noreferrer"><p className='para'>MEAL RECIPE FINDER (click)</p><img src={p6} alt="" className='worksImg' /></a>
    <a href="http://parallax-scrolling-19.netlify.app" target="_blank" rel="noopener noreferrer"><p className='para'>PARALLAX SCROLLING EFFECT (click) </p><img src={p2} alt="" className='worksImg'/> </a>
    <a href="http://dark-19light.netlify.app" target="_blank" rel="noopener noreferrer"><p className='para'>DARK MODE EFFECT (click) </p><img src={p3} alt="" className='worksImg'/></a>
    <div><p className='para'>KNIGHT BITE FOOD WEBSITE </p><img src={p5} alt="" className='worksImg'/></div>
